621a891bb019070df5d0cecb7d18c1c988f9a6cc
Davia Belinda <daviabelinda@gmail.com>
2020-03-23 12:37:34 +0700

    asalam

--- cobain/ViewController.swift
+++ cobain/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        var a = ""
+        var b = ""
         // Do any additional setup after loading the view.
     }
 
