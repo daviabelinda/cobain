3761eddcf7cb389b9bdb784abb8ebc93246257db
Davia Belinda <daviabelinda@gmail.com>
2020-03-23 12:36:58 +0700

    Initial Commit

---
+++ cobain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cobain
+//
+//  Created by Davia Belinda Hidayat on 23/03/20.
+//  Copyright © 2020 Davia Belinda Hidayat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
